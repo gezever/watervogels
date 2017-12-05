79501ce8bfc8e99796e7c38e980e9269b43fa4af
gehau <gezever@skynet.be>
2017-12-05 16:16:00 +0100

    plot vanuit klik op geojson

--- app.R
+++ app.R
@@ -2,50 +2,17 @@ library(shiny)
 library(leaflet)
 library(RColorBrewer)
 library(ggplot2)
-library(leaflet.extras)
 library(sp)
 require(reshape)
 library(jsonlite)
+source('global.R', local = TRUE)
+
+
 
 #
 # zie https://rstudio.github.io/leaflet/shiny.html
 # klikken op een bol en dan plot: http://www.r-graph-gallery.com/2017/03/14/4-tricks-for-working-with-r-leaflet-and-shiny/
 #
-
-topoData <- readLines("data/V3/localities.geojson")
-topoData.df <- as.data.frame(fromJSON(topoData))
-waarnemingen <- readRDS("data/V3/occurrence.rds")
-# waarnemingen <-  waarnemingen[waarnemingen$municipality == "Kalmthout" |
-#                  waarnemingen$municipality == "Brecht" |
-#                  waarnemingen$municipality == "Schoten",]
-# waarnemingen <-  waarnemingen[waarnemingen$verbatimLocality == "De Moerkens KALMTHOUT" |
-#                  waarnemingen$verbatimLocality == "Stappersven KALMTHOUT" |
-#                  waarnemingen$verbatimLocality == "Drielingenven KALMTHOUT"  |
-#                  waarnemingen$verbatimLocality == "Biezenkuilen KALMTHOUT" , ]
-
-date_split <-
-  colsplit(waarnemingen$eventDate,
-           split = "-",
-           names = c('jaar', 'maand', 'tmp'))
-day_split <-
-  colsplit(date_split$tmp,
-           split = "T",
-           names = c('dag', 'tijd'))
-jaar <- as.character(date_split$jaar)
-maand <- as.character(date_split$maand)
-dag <- as.character(day_split$dag)
-tijd <- as.character(day_split$tijd)
-waarnemingen <- cbind(waarnemingen, jaar, maand, dag, tijd)
-
-
-
-# we sorteren hier Descending, zodat de kleinste bollen bovenaan liggen
-waarnemingen <- waarnemingen[order(-waarnemingen$individualCount),]
-
-waarnemers <-
-  read.csv(file = "data/waarnemers_kalmthout.csv", header = TRUE)
-
-
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
@@ -60,6 +27,7 @@ ui <- bootstrapPage(
       max(as.Date(waarnemingen$eventDate)),
       value = range(as.Date(waarnemingen$eventDate))
     ),
+    checkboxInput("natuurgebieden", "Toon kaartlaag met natuurgebieden", TRUE),
     plotOutput("plot2"),
     selectInput("soort", "Kies de soort", choices = sort(unique(
       waarnemingen$vernacularName
@@ -67,14 +35,19 @@ ui <- bootstrapPage(
     selectInput("waarnemer", "kies de waarnemer", choices =
                   waarnemers),
     checkboxInput("legend", "Show legend", TRUE),
+    
     textOutput("message", container = h3)
   )
 )
 
 server <- function(input, output, session) {
-  data_of_click_on_geojson <- reactiveValues(msg = NULL)
-  
+  source('functions.R', local = TRUE)
+ 
+  waarnemingen_in <- reactiveValues(clickedGeojson = NULL)
   data_of_click <- reactiveValues(clickedMarker = NULL)
+  
+ 
+  
   
   # Reactive expression for the data subsetted to what the user selected
   
@@ -89,9 +62,6 @@ server <- function(input, output, session) {
                      waarnemingen$vernacularName == input$soort &
                      grepl(input$waarnemer, waarnemingen$identifiedBy),]
     }
-    
-    
-    #waarnemingen[waarnemingen$vernacularName== input$soort,]
   })
   # This reactive expression represents the palette function,
   # which changes as the user makes selections in UI.
@@ -106,12 +76,7 @@ server <- function(input, output, session) {
     
     leaflet() %>%
       addTiles(attribution = 'Data <a href="http://dataset.inbo.be/watervogels-occurrences">http://dataset.inbo.be/watervogels-occurrences</a>') %>%
-      addGeoJSON(topoData,
-                 weight = 1,
-                 color = "#444444",
-                 fill = TRUE) %>%
-      
-      #addTiles(        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'      ) %>%
+      #addGeoJSON(topoData,  weight = 1,   color = "#444444", fill = TRUE) %>%
       setView(lng = 4.5809,
               lat = 51.3535,
               zoom = 12)
@@ -127,71 +92,19 @@ server <- function(input, output, session) {
       clearShapes() %>%
       clearMarkerClusters() %>%
       clearMarkers() %>%
-      addCircleMarkers(
-        radius = ~ log(individualCount + 1) * 10 ,
-        weight = 1,
-        layerId =  ~ id,
-        lng = ~ decimalLongitude,
-        lat = ~ decimalLatitude,
-        color = "#777777",
-        fillColor = ~ pal(individualCount),
-        fillOpacity = 0.7,
-        popup = ~ paste(
-          verbatimLocality,
-          municipality,
-          stateProvince,
-          "<br/>",
-          "aantal",
-          vernacularName,
-          individualCount,
-          "<br/>",
-          "datum",
-          eventDate,
-          "<br/>",
-          "waarnemer(s)",
-          identifiedBy ,
-          "<br/>",
-          samplingProtocol,
-          "<br/>",
-          samplingEffort
-        ),
+      addCircleMarkers( radius = ~ log(individualCount + 1) * 10 ,  weight = 1,    layerId =  ~ id,    lng = ~ decimalLongitude,  lat = ~ decimalLatitude,  color = "#777777",  fillColor = ~ pal(individualCount),    fillOpacity = 0.7,
+        popup = ~ paste(verbatimLocality,municipality,stateProvince,"<br/>","aantal",vernacularName,individualCount,"<br/>","datum",eventDate,"<br/>","waarnemer(s)",  identifiedBy , "<br/>",samplingProtocol, "<br/>",samplingEffort),
         clusterOptions = markerClusterOptions()
       )
     #addWebGLHeatmap(lng = ~decimalLongitude, lat = ~decimalLatitude, intensity = ~log(individualCount+1)/100,size = 10000 )
     
   })
   
-  ################## Plot on click
-  # store the click
-  observeEvent(input$map_marker_click, {
-    data_of_click$clickedMarker <- input$map_marker_click
-  })
   
-  observeEvent(input$map_geojson_click, {
-    topodata_selected_object <- subset(topoData.df, topoData.df$features.properties$code == input$map_geojson_click$properties$code)
-    polygon_matrix <- matrix(unlist(topodata_selected_object$features.geometry$coordinates),ncol = 2)
-    waarnemingen_in_geojson_object <- subset(waarnemingen, sp::point.in.polygon(c(waarnemingen$decimalLatitude),c(waarnemingen$decimalLongitude), polygon_matrix[,2], polygon_matrix[,1]) == 1 )
-    data_of_click_on_geojson$msg <- waarnemingen_in_geojson_object$decimalLongitude[1]
-  })
+
   
-  output$message <- renderText(data_of_click_on_geojson$msg)
-  output$plot2 <- renderPlot({
-    my_waarnemingen <-
-      subset(waarnemingen,
-             waarnemingen$id == data_of_click$clickedMarker$id)
-    my_lat <- head(my_waarnemingen$decimalLatitude, 1)
-    my_lon <- head(my_waarnemingen$decimalLongitude, 1)
-    my_filteredwaarnemingen <-
-      waarnemingen[as.Date(waarnemingen$eventDate) >= as.Date(input$tijdstip[1]) &
-                     as.Date(waarnemingen$eventDate) <= as.Date(input$tijdstip[2]) &
-                     waarnemingen$decimalLongitude == my_lon &
-                     waarnemingen$decimalLatitude == my_lat &
-                     waarnemingen$vernacularName == input$soort,]
-    my_location <-
-      paste(
-        my_filteredwaarnemingen$verbatimLocality[1],
-        my_filteredwaarnemingen$municipality[1]
-      )
+  
+  plot_waarnemingen <- function(wnm,locatie){
     
     #http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html
     # By default, geom_bar() has the stat set to count.
@@ -199,7 +112,7 @@ server <- function(input, output, session) {
     # In order to make a bar chart create bars instead of histogram, you need to do two things.
     #     1. Set stat=identity
     #     2. Provide both x and y inside aes() where, x is either character or factor and y is numeric.
-    p <- ggplot(data = my_filteredwaarnemingen, aes(x = jaar, y = individualCount)) + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+    p <- ggplot(data = wnm, aes(x = jaar, y = individualCount)) + theme(axis.text.x = element_text(angle = 60, hjust = 1))
     
     # add geom
     p <- p + geom_bar(stat = 'identity', width = .5)
@@ -208,13 +121,80 @@ server <- function(input, output, session) {
     p <- p %+% aes(fill = maand)
     
     # titel en subtitel
-    p <-  p %+% labs(subtitle = my_location,  title = input$soort)
-    p
+    p <-  p %+% labs(subtitle = locatie,  title = input$soort)
+    return(p)
+  }
+  
+  ################## Plot on click
+  # store the click
+  observeEvent(input$map_marker_click, {
+    data_of_click$clickedMarker <- input$map_marker_click
+    #output$message <- renderText(paste(waarnemingen_in$clickedGeojson$decimalLatitude[1],data_of_click$clickedMarker$id))
+    output$plot2 <- renderPlot({
+      my_waarnemingen <-  subset(waarnemingen, waarnemingen$id == data_of_click$clickedMarker$id)
+      #my_waarnemingen <-  subset(waarnemingen, waarnemingen$id == data_of_click$clickedMarker$id)
+      my_lat <- head(my_waarnemingen$decimalLatitude, 1)
+      my_lon <- head(my_waarnemingen$decimalLongitude, 1)
+      my_filteredwaarnemingen <-
+        waarnemingen[as.Date(waarnemingen$eventDate) >= as.Date(input$tijdstip[1]) &
+                       as.Date(waarnemingen$eventDate) <= as.Date(input$tijdstip[2]) &
+                       waarnemingen$decimalLongitude == my_lon &
+                       waarnemingen$decimalLatitude == my_lat &
+                       waarnemingen$vernacularName == input$soort,]
+      
+      my_location <-paste(
+        my_filteredwaarnemingen$verbatimLocality[1],
+        my_filteredwaarnemingen$municipality[1]
+      )
+      p <- plot_waarnemingen(my_filteredwaarnemingen,my_location) 
+      p
+      
+    })
+  })
+  observeEvent(input$map_geojson_click, {
+    topodata_selected_object <- subset(topoData.df, topoData.df$features.properties$code == input$map_geojson_click$properties$code)
+    polygon_matrix <- matrix(unlist(topodata_selected_object$features.geometry$coordinates),ncol = 2)
+    waarnemingen_in$clickedGeojson  <- subset(waarnemingen, sp::point.in.polygon(c(waarnemingen$decimalLatitude),c(waarnemingen$decimalLongitude), polygon_matrix[,2], polygon_matrix[,1]) == 1 )
+    output$plot2 <- renderPlot({
+      my_waarnemingen <- waarnemingen_in$clickedGeojson
+      #my_waarnemingen <-  subset(waarnemingen, waarnemingen$id == data_of_click$clickedMarker$id)
+      my_lat <- head(my_waarnemingen$decimalLatitude, 1)
+      my_lon <- head(my_waarnemingen$decimalLongitude, 1)
+      my_filteredwaarnemingen <-
+        waarnemingen[as.Date(waarnemingen$eventDate) >= as.Date(input$tijdstip[1]) &
+                       as.Date(waarnemingen$eventDate) <= as.Date(input$tijdstip[2]) &
+                       waarnemingen$decimalLongitude == my_lon &
+                       waarnemingen$decimalLatitude == my_lat &
+                       waarnemingen$vernacularName == input$soort,]
+      
+      my_location <-paste(
+        my_filteredwaarnemingen$verbatimLocality[1],
+        my_filteredwaarnemingen$municipality[1]
+      )
+      
+      
+      p <- plot_waarnemingen(my_filteredwaarnemingen,my_location) 
+      p
+      
+    })
     
+  })
+ 
+
+  # Use a separate observer to add geojson objects.
+  observe({
+    proxy <- leafletProxy("map", data = waarnemingen)
+    # Remove any existing legend, and only if the legend is
+    # enabled, create a new one.
+    proxy %>% clearGeoJSON()
+    if (input$natuurgebieden) {
+      proxy %>% 
+        addGeoJSON(topoData,  weight = 1,   color = "#444444", fill = TRUE) 
+    }
   })
   
  
-  
+
   # Use a separate observer to recreate the legend as needed.
   observe({
     proxy <- leafletProxy("map", data = waarnemingen)
