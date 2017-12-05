dec98fe852d28b6982593c16f576bdffb907aed8
gehau <gezever@skynet.be>
2017-12-05 16:14:44 +0100

    geojson objecten zijn klikbaar en  er wordt een plot gemaakt

---
+++ global.R
@@ -0,0 +1,35 @@
+
+topoData <- readLines("data/V3/localities.geojson")
+topoData.df <- as.data.frame(fromJSON(topoData))
+waarnemingen <- readRDS("data/V3/occurrence.rds")
+waarnemingen <-  waarnemingen[waarnemingen$municipality == "Kalmthout" |
+                                waarnemingen$municipality == "Brecht" |
+                                waarnemingen$municipality == "Schoten",]
+# waarnemingen <-  waarnemingen[waarnemingen$verbatimLocality == "De Moerkens KALMTHOUT" |
+#                  waarnemingen$verbatimLocality == "Stappersven KALMTHOUT" |
+#                  waarnemingen$verbatimLocality == "Drielingenven KALMTHOUT"  |
+#                  waarnemingen$verbatimLocality == "Biezenkuilen KALMTHOUT" , ]
+
+date_split <-
+  colsplit(waarnemingen$eventDate,
+           split = "-",
+           names = c('jaar', 'maand', 'tmp'))
+day_split <-
+  colsplit(date_split$tmp,
+           split = "T",
+           names = c('dag', 'tijd'))
+jaar <- as.character(date_split$jaar)
+maand <- as.character(date_split$maand)
+dag <- as.character(day_split$dag)
+tijd <- as.character(day_split$tijd)
+waarnemingen <- cbind(waarnemingen, jaar, maand, dag, tijd)
+
+
+
+# we sorteren hier Descending, zodat de kleinste bollen bovenaan liggen
+waarnemingen <- waarnemingen[order(-waarnemingen$individualCount),]
+
+waarnemers <-
+  read.csv(file = "data/waarnemers_kalmthout.csv", header = TRUE)
+
+
